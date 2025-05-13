41b5e32836846d4ff80118e9d4c8fa7c3c497718
Dor Furman <furmandevs@gmail.com>
2025-05-13 20:37:41 +0300

    Initial commit - removed default create-react-app files

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styled from 'styled-components';
+import { GridCities } from './components/GridCities';
 
-function App() {
+const AppView = styled.div`
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+
+  *, *::before, *::after {
+    box-sizing: border-box;
+    margin: 0;
+    font-family: 'Roboto', sans-serif;
+  }
+
+  html, body {
+    padding: 0;
+  }
+`;
+
+export const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AppView>
+      <GridCities />
+    </AppView>
   );
 }
-
-export default App;
